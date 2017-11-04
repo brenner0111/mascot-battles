862ba285b9d547dc0599209c8f93d0e21e197549
Brenner Harris <brennerharris7@yahoo.com>
2017-11-04 16:43:31 -0400

    added networking package

--- core/src/com/mygdx/game/MascotBattles.java
+++ core/src/com/mygdx/game/MascotBattles.java
@@ -3,19 +3,38 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MascotBattles extends ApplicationAdapter {
+	private final float GAME_WORLD_WIDTH = 1080;
+	private final float GAME_WORLD_HEIGHT = 720;
+	
 	SpriteBatch batch;
 	Texture img;
+	
+	private OrthographicCamera camera;
+	private Viewport viewport;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		viewport =  new StretchViewport(GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT, camera);
+		viewport.apply();
+		camera.position.set(GAME_WORLD_WIDTH/2, GAME_WORLD_HEIGHT/2, 0);
 	}
-
+	
+	@Override
+	public void resize(int width, int height) {
+	    viewport.update(width, height);
+	    camera.position.set(GAME_WORLD_WIDTH/2, GAME_WORLD_HEIGHT/2, 0);
+	}
+	
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
